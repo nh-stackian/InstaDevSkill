28905cb3a446945ff645b4f2bade8b0d3d50ad65
NH <nh.stackian@gmail.com>
2021-01-17 21:00:12 +0600

    make tags table  migration

--- src/database/migrations/2021_01_17_143017_create_tags_table.php
+++ src/database/migrations/2021_01_17_143017_create_tags_table.php
@@ -15,8 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->timestamps();
+            $table->string('name');
         });
     }
 
